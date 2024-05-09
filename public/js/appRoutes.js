dd119f14e4c7f227f7d3593e4c2c08e90a8844ec
neraization <maslo.nermin@gmail.com>
2024-05-10 01:57:48 +0200

    First part of application is implemented in version 1.0.0

---
+++ public/js/appRoutes.js
@@ -0,0 +1,12 @@
+angular.module('appRoutes', []).config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
+
+	$routeProvider
+
+		.when('/', {
+			templateUrl: 'views/login.html',
+			controller: 'LoginController'
+		})
+
+	$locationProvider.html5Mode(true);
+
+}]);
